2140a38b75f66f0467719314e734636c70c64c85
sycodes95 <sycodes95@gmail.com>
2023-09-06 17:22:09 -0700

    created async function to fetch openai api

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState('')
+  const [msgs, setMsgs] = useState([])
+
+  useEffect(()=>{
+    const handleSend = async () => {
+      try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', {
+          method: 'POST',
+          headers: {
+            'Authorization': `Bearer ${import.meta.env.VITE_OPEN_AI_API_KEY}`,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            messages: [...msgs, { role: 'user', content: input }],
+            model: "gpt-3.5-turbo",
+            temperature: 1,
+            max_tokens: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+          })
+        });
+
+        const data = await response.json();
+        
+        console.log(data);
+        const assistantMessage = data.choices[0].message.content;
+        setMsgs([...msgs, { role: 'user', content: input }, { role: 'assistant', content: assistantMessage }]);
+        setInput('');
+
+      } catch (error) {
+        console.error("Error communicating with OpenAI:", error);
+      }
+    };
+    handleSend()
+  },[])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
